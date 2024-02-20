4df36f2a5bd413e9b0c3218afda188abd93bd2c1
阿花 <cvsq@icloud.com>
2024-02-20 23:12:31 +0800

    uplaod

---
+++ types/is.d.ts
@@ -0,0 +1,12 @@
+
+/** 是否 */
+type IS = {
+    /**
+     * @description 是否空数组
+     * @param { unknown } value
+     * @return { boolean }
+     */
+    emptyArray: (value: unknown) => boolean
+}
+
+export = IS
---
+++ types/to.d.ts
@@ -0,0 +1,19 @@
+
+/** 转换 */
+type TO = {
+    /**
+         * @description 转换为字符串
+         * @param { unknown } value
+         * @return { string }
+         */
+    string: (value: unknown) => string
+
+    /**
+     * @description 转换为数字
+     * @param { unknown } value
+     * @return { number }
+     */
+    number: (value: unknown) => number
+}
+
+export = TO
